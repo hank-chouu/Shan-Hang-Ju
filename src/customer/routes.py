e14174a08dbfe8c8c35a19db4fb58258b5375946
Heng Tse Chou <legohank1219@gmail.com>
2023-01-18 16:06:34 +0800

    home page

--- src/customer/routes.py
+++ src/customer/routes.py
@@ -2,6 +2,12 @@ from flask import Blueprint, render_template
 
 customer = Blueprint('customer', __name__)
 
+
+@customer.route('', methods = ['GET'])
+def home():
+
+    return render_template('home.html')
+
 @customer.route('/reservation', methods = ['GET', 'POST'])
 def reservation():
 
