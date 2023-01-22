c6eee1d78d5649d5d88489690629be5510b514ec
Heng Tse Chou <legohank1219@gmail.com>
2023-01-22 19:35:25 +0800

    refactor into 1 room.html

--- src/customer/routes.py
+++ src/customer/routes.py
@@ -1,7 +1,21 @@
 from flask import Blueprint, render_template, redirect, url_for
+from markupsafe import escape
 
 customer = Blueprint('customer', __name__)
 
+## functions
+
+def img_indexing(room_num, img_amount:int):
+    idx = {}
+    for i in range(img_amount):
+        idx[i] = ''.join([str(room_num), '_', str(i+2), '.jpg'])
+    return idx
+
+pricing = {'2 beds':['NT4,100', 'NT2,000', 'NT2,300', 'NT4,100'], 
+           '3 beds':['NT4,500', 'NT2,800', 'NT3,300', 'NT4,500'], 
+           '4 beds':['NT6,300', 'NT3,100', 'NT3,800', 'NT6,300']}
+
+## routes
 
 @customer.route('/home', methods = ['GET'])
 def home():
@@ -21,30 +35,67 @@ def location():
 ## rooms
 
 @customer.route('/rooms', methods = ['GET'])
-def rooms_type():
+def rooms_all():
 
-    return render_template('rooms_view.html')
+    return render_template('rooms_all.html')
 
-@customer.route('/rooms/301', methods = ['GET'])
-def room301():
+@customer.route('/rooms/<int:room_num>', methods = ['GET'])
+def room_each(room_num):
 
-    return render_template('room301.html')
+    if room_num == 301: 
+
+        img_amount = 5
+        photo_index = img_indexing(room_num, img_amount)
+
+        return render_template('rooms_each.html', title = '杉行街', 
+                                                  header = '杉行街（四人房）', 
+                                                  imgs = photo_index, 
+                                                  add = 1, 
+                                                  pricing = pricing['4 beds'])
+    elif room_num == 302: 
+
+        img_amount = 2
+        photo_index = img_indexing(room_num, img_amount)
+
+        return render_template('rooms_each.html', title = '君子巷', 
+                                                  header = '君子巷（雙人房）', 
+                                                  imgs = photo_index, 
+                                                  add = 0, 
+                                                  pricing = pricing['2 beds'])    
+    elif room_num == 303: 
+
+        img_amount = 2
+        photo_index = img_indexing(room_num, img_amount)
+
+        return render_template('rooms_each.html', title = '後車巷', 
+                                                  header = '後車巷（雙人房）', 
+                                                  imgs = photo_index, 
+                                                  add = 1, 
+                                                  pricing = pricing['2 beds'])    
+    elif room_num == 501: 
+
+        img_amount = 3
+        photo_index = img_indexing(room_num, img_amount)
+
+        return render_template('rooms_each.html', title = '桂花巷', 
+                                                  header = '桂花巷（雙人房）', 
+                                                  imgs = photo_index, 
+                                                  add = 0, 
+                                                  pricing = pricing['2 beds'])
+    elif room_num == 502: 
+
+        img_amount = 4
+        photo_index = img_indexing(room_num, img_amount)
+
+        return render_template('rooms_each.html', title = '九曲巷', 
+                                                  header = '九曲巷（三人房）', 
+                                                  imgs = photo_index, 
+                                                  add = 1, 
+                                                  pricing = pricing['3 beds'])
+    else: 
+        return 'PAGE NOT FOUND. ROOM NUMBER IS NOT VALID. '
 
 
-@customer.route('/rooms/302', methods = ['GET'])
-def room302():
-
-    return render_template('room302.html')
-
-@customer.route('/rooms/501', methods = ['GET'])
-def room501():
-
-    return render_template('room501.html')
-
-@customer.route('/rooms/502', methods = ['GET'])
-def room502():
-
-    return render_template('room502.html')
 
 ## reservation
 
