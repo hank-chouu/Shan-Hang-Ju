26ee087129a32a625f9e1634c5da8904caaecebb
Heng Tse Chou <legohank1219@gmail.com>
2023-01-26 03:52:10 +0800

    search result, logger

--- src/customer/routes.py
+++ src/customer/routes.py
@@ -1,8 +1,11 @@
-from flask import Blueprint, render_template, redirect, url_for
-from datetime import datetime
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from datetime import datetime, timezone, timedelta
 from markupsafe import escape
+# import pytz
+import pandas as pd
 
-from src.extensions.models import db, Rooms
+
+from src.extensions.models import db, Rooms, allLogger
 
 
 
@@ -16,9 +19,14 @@ def img_indexing(room_num, img_amount:int):
         idx[i] = ''.join([str(room_num), '_', str(i+2), '.jpg'])
     return idx
 
+tz = timezone(timedelta(hours=+8))
+
 pricing = {'2 beds':['NT4,100', 'NT2,000', 'NT2,300', 'NT4,100'], 
            '3 beds':['NT4,500', 'NT2,800', 'NT3,300', 'NT4,500'], 
            '4 beds':['NT6,300', 'NT3,100', 'NT3,800', 'NT6,300']}
+
+names = {'301':'杉行街（四人房）', '302':'君子巷（雙人房）', '303':'後車巷（雙人房）',
+         '501':'桂花巷（雙人房）', '502':'九曲巷（三人房）'}
 
 ## routes
 
@@ -52,8 +60,8 @@ def rooms_each(room_num):
         img_amount = 5
         photo_index = img_indexing(room_num, img_amount)
 
-        return render_template('rooms_each.html', title = '杉行街', 
-                                                  header = '杉行街（四人房）', 
+        return render_template('rooms_each.html', title = names[str(room_num)][0:3], 
+                                                  header = names[str(room_num)], 
                                                   imgs = photo_index, 
                                                   add = 1, 
                                                   pricing = pricing['4 beds'])
@@ -62,8 +70,8 @@ def rooms_each(room_num):
         img_amount = 2
         photo_index = img_indexing(room_num, img_amount)
 
-        return render_template('rooms_each.html', title = '君子巷', 
-                                                  header = '君子巷（雙人房）', 
+        return render_template('rooms_each.html', title = names[str(room_num)][0:3], 
+                                                  header = names[str(room_num)], 
                                                   imgs = photo_index, 
                                                   add = 0, 
                                                   pricing = pricing['2 beds'])    
@@ -72,8 +80,8 @@ def rooms_each(room_num):
         img_amount = 2
         photo_index = img_indexing(room_num, img_amount)
 
-        return render_template('rooms_each.html', title = '後車巷', 
-                                                  header = '後車巷（雙人房）', 
+        return render_template('rooms_each.html', title = names[str(room_num)][0:3], 
+                                                  header = names[str(room_num)][0:3], 
                                                   imgs = photo_index, 
                                                   add = 1, 
                                                   pricing = pricing['2 beds'])    
@@ -82,8 +90,8 @@ def rooms_each(room_num):
         img_amount = 3
         photo_index = img_indexing(room_num, img_amount)
 
-        return render_template('rooms_each.html', title = '桂花巷', 
-                                                  header = '桂花巷（雙人房）', 
+        return render_template('rooms_each.html', title = names[str(room_num)][0:3], 
+                                                  header = names[str(room_num)], 
                                                   imgs = photo_index, 
                                                   add = 0, 
                                                   pricing = pricing['2 beds'])
@@ -92,8 +100,8 @@ def rooms_each(room_num):
         img_amount = 4
         photo_index = img_indexing(room_num, img_amount)
 
-        return render_template('rooms_each.html', title = '九曲巷', 
-                                                  header = '九曲巷（三人房）', 
+        return render_template('rooms_each.html', title = names[str(room_num)][0:3], 
+                                                  header = names[str(room_num)], 
                                                   imgs = photo_index, 
                                                   add = 1, 
                                                   pricing = pricing['3 beds'])
@@ -107,18 +115,115 @@ def rooms_each(room_num):
 @customer.route('/reservation', methods = ['GET', 'POST'])
 def reservation():
 
-    return render_template('reservation.html')
+    if request.method == 'POST':
+        if request.form.get('search') == 'search':
+            check_in = request.form.get('check_in_date')
+            check_out = request.form.get('check_out_date')
+            room_type = request.form.get('room_type')
+
+            if check_in != '' and check_out != '':
+
+                check_in_dt = datetime.strptime(check_in, '%Y/%m/%d').replace(tzinfo=tz)
+                check_out_dt = datetime.strptime(check_out, '%Y/%m/%d').replace(tzinfo=tz)
+
+                # filter by date
+                
+                query = db.session.query(Rooms).filter(
+                        Rooms.date >= check_in_dt, 
+                        Rooms.date < check_out_dt
+                ).all()
+
+                # check if rooms are available
+
+                available_rooms = []
+                total_days = (check_out_dt.date() - check_in_dt.date()).days
+                
+                if room_type == '2':
+                    room_302, room_303, room_501 = 0, 0, 0
+                    for rooms in query:
+                        room_302 += rooms.room_302
+                        room_303 += rooms.room_303
+                        room_501 += rooms.room_501                
+                    if room_302 == total_days:
+                        available_rooms.append('302')
+                    if room_501 == total_days:
+                        available_rooms.append('501')
+                    if room_303 == total_days:
+                        available_rooms.append('303')
+                    
+
+                elif room_type == '3':
+                    room_502 = 0
+                    for rooms in query:
+                        room_502 += rooms.room_502
+                    if room_502 == total_days:
+                        available_rooms.append('502')
+
+                elif room_type == '4':
+                    room_301 = 0
+                    for rooms in query:
+                        room_301 += rooms.room_301
+                    if room_301 == total_days:
+                        available_rooms.append('301')
+
+                # generate available room info
+                if len(query) == 0:
+                    allLogger.warn('Date-Room database is runned out.')
+                    flash('資料庫內部異常，無法查詢', category='error')
+                    return render_template('reservation.html', result = False)
+                elif len(available_rooms) == 0:
+                    is_available = False
+                    info = [check_in.replace('/', '-'), check_out.replace('/', '-')]
+                elif len(available_rooms) != 0:
+                    is_available = True
+
+                    # prepare price table
+                    date_pricing = pd.read_csv('src/files/date_pricing.csv')
+                    date_pricing['date_dt'] = pd.to_datetime(date_pricing['date'], format='%Y/%m/%d')
+                    date_pricing['date_dt'] = date_pricing['date_dt'].dt.tz_localize('UTC').dt.tz_convert('Asia/Taipei')
+                    date_pricing = date_pricing[(date_pricing.date_dt >= pd.Timestamp(check_in_dt)) & (date_pricing.date_dt < pd.Timestamp(check_out_dt))]
+
+                    info = {}
+                    for room in available_rooms:
+                        room_detail = {}
+                        room_detail['img'] = room + '_1.jpg'
+                        room_detail['name'] = names[room]
+                        room_detail['dates'] = [check_in.replace('/', '-'), check_out.replace('/', '-'), len(date_pricing)]
+                        room_detail['amount'] = date_pricing[room].sum()
+                        info[room] = room_detail
+
+                return render_template('reservation.html', result = True, 
+                                                           available = is_available, 
+                                                           info = info)
+                        
+    return render_template('reservation.html', result = False)
 
 
 @customer.route('/add', methods=["GET"])
 def add_row():
 
-    date = datetime.now().strftime('%Y-%m-%d')
-    row = Rooms(date, 1, 1, 1, 1, 1)
-    db.session.add(row)
+    today_date = '2023/01/22'
+    today_date = datetime.strptime(today_date, '%Y/%m/%d').replace(tzinfo=tz)
+
+    for i in range(10):
+
+        row = Rooms(today_date, 1, 1, 1, 1, 1)
+        db.session.add(row)
+        today_date = today_date + timedelta(days=1)
     db.session.commit()
+    # check_in = '2023/01/24'
+    # check_out = '2023/01/25'
+    # check_in = datetime.strptime(check_in, '%Y/%m/%d').replace(tzinfo=tz)
+    # check_out = datetime.strptime(check_out, '%Y/%m/%d').replace(tzinfo=tz)
 
-
+    # # print(today_date)
+    # query = db.session.query(Rooms).filter(
+    #     Rooms.date >= check_in, 
+    #     Rooms.date < check_out
+    # ).all()
+    # for row in query:
+    #     print(row.date.replace(tzinfo=tz))
+    # db.session.commit()
 
 
     return 'OK'
--- src/extensions/models.py
+++ src/extensions/models.py
@@ -1,5 +1,9 @@
 from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+import logging
+import os
+import pytz
+from datetime import datetime
 
 ## users
 
@@ -12,7 +16,7 @@ db = SQLAlchemy()
 
 class Rooms(db.Model):
     __tablename__ = 'rooms'
-    date = db.Column(db.String(15), primary_key = True)
+    date = db.Column(db.DateTime, primary_key = True)
     room_301 = db.Column(db.Integer, nullable = False)
     room_302 = db.Column(db.Integer, nullable = False)
     room_303 = db.Column(db.Integer, nullable = False)
@@ -26,3 +30,62 @@ class Rooms(db.Model):
         self.room_303 = room_303
         self.room_501 = room_501
         self.room_502 = room_502
+
+
+## logger
+
+class Formatter(logging.Formatter):
+    """override logging.Formatter to use an aware datetime object"""
+
+    def converter(self, timestamp):
+        # Create datetime in UTC
+        dt = datetime.fromtimestamp(timestamp, tz=pytz.UTC)
+        # Change datetime's timezone
+        return dt.astimezone(pytz.timezone('Asia/Taipei'))
+
+    def formatTime(self, record, datefmt=None):
+        dt = self.converter(record.created)
+        if datefmt:
+            s = dt.strftime(datefmt)
+        else:
+            try:
+                s = dt.isoformat(timespec='milliseconds')
+            except TypeError:
+                s = dt.isoformat()
+        return s
+
+
+def logger():
+    #1.setup log path and create log directory
+    logName = 'Program.log'
+    logDir = 'log'
+    logPath = logDir + '/' + logName
+
+    #create log directory 
+    os.makedirs(logDir,exist_ok=True)
+
+    #2.create logger, then setLevel
+    global allLogger
+    allLogger = logging.getLogger('allLogger')
+    allLogger.setLevel(logging.DEBUG)
+
+    #3.create file handler, then setLevel
+    #create file handler
+    fileHandler = logging.FileHandler(logPath,mode='w')
+    fileHandler.setLevel(logging.INFO)
+
+    #4.create stram handler, then setLevel
+    #create stream handler
+    streamHandler = logging.StreamHandler()
+    streamHandler.setLevel(logging.INFO)
+
+    #5.create formatter, then handler setFormatter
+    AllFormatter = Formatter("%(asctime)s - [line:%(lineno)d] - %(levelname)s: %(message)s", '%Y-%m-%d %H:%M:%S')
+    fileHandler.setFormatter(AllFormatter)
+    streamHandler.setFormatter(AllFormatter)
+
+    #6.logger addHandler
+    allLogger.addHandler(fileHandler)
+    allLogger.addHandler(streamHandler)
+
+logger()
