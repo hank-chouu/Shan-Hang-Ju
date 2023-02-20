11df5b5b8b373018bcd89bba6ee4349292259ec6
Heng Tse Chou <legohank1219@gmail.com>
2023-02-21 00:02:04 +0800

    feat: change mail context

--- src/extensions/email.py
+++ src/extensions/email.py
@@ -8,11 +8,20 @@ mail = Mail()
 # generate mail for confirmation
 
 def msg_body(info):
-    text_msg = '訂購房型：{}\n入住日期：{}\n退房日期：{}\n\n您這次訂單款項一共 NT{} 元，\n請在三日內將訂金 NT{} 元轉至管家帳戶，\n並於入住時繳清餘款共 NT{} 元\n\n'.format(
-        info['room'], info['check_in'], info['check_out'], info['total'], info['deposit'], info['final']
-    )
 
-    text_msg = text_msg + '銀行代碼：\n帳號：\n\n謝謝您！期待與您在鹿港相見！\n\n杉行居 敬上'
+    text_msg = """
+    <p>訂購房型：{}<br>入住日期：{}<br>退房日期：{}<p>
+    <p>您這次訂單款項一共 NT{} 元，<br>請在三日內將訂金 NT{} 元匯至管家帳戶，<br>並透過臉書粉專或來電告知，<br>再請於入住時繳清餘款共 NT{} 元。<p>
+    <p>匯款銀行：鹿港信用合作社 （165）<br>銀行帳號：00019152248820<br>匯款戶名：黃淑汝
+    <p>謝謝您！期待與您在鹿港相見！<br>以下為我們的連絡資訊：<br>電話：04-7756852<br>地址：彰化縣鹿港鎮杉行街77號<br>臉書：<a href='https://www.facebook.com/shanhanju'>杉行居/ SHAN HANG JU</a><p>
+    <p>杉行居 敬上<p>
+    """.format(info['room'], 
+               info['check_in'], 
+               info['check_out'], 
+               "{:,}".format(info['total']), 
+               "{:,}".format(info['deposit']), 
+               "{:,}".format(info['final']))
+
 
     return text_msg
 
@@ -27,6 +36,6 @@ def create_msg(recipient, info):
     msg = Message(msg_title,
                   sender=msg_sender,
                   recipients=msg_recipients)
-    msg.body = body
+    msg.html = body
 
     return msg
